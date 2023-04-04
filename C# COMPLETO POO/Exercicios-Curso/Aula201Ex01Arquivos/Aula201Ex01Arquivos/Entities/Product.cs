09ef584f1fb383b9eb10373f8a951a3f3d264d0e
Guilherme Dias de Freitas <gui_dias.freitas@hotmail.com>
2023-04-04 09:54:05 -0300

    Arrumando Pastas

---
+++ C# COMPLETO POO/Exercicios-Curso/Aula201Ex01Arquivos/Aula201Ex01Arquivos/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula201Ex01Arquivos.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double value, int quantity)
+        {
+            Name = name;
+            Value = value;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Value * Quantity;
+        }
+    }
+}
---
+++ C# COMPLETO POO/Exercicios-Curso/Aula201Ex01Arquivos/Aula201Ex01Arquivos/Program.cs
@@ -0,0 +1,48 @@
+﻿using Aula201Ex01Arquivos.Entities;
+using System.Globalization;
+
+namespace Aula201Ex01Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o caminho para o arquivo: ");
+            string sourceFilePath = Console.ReadLine();
+
+            string[] lines = File.ReadAllLines(sourceFilePath);
+
+            string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+            string targetFolderPath = sourceFolderPath + @"\out";
+            string targetFilePath = targetFolderPath + @"\summary.csv";
+
+            Directory.CreateDirectory(targetFolderPath);
+
+            try
+            {
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(",");
+                        string name = fields[0];
+                        double value = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, value, quantity);
+
+                        sw.WriteLine(product.Name + ", " + product.Total().ToString("F2", CultureInfo.InvariantCulture));
+
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: ");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
